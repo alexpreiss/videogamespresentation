22da49c5afc76f212c21facafd7d471ecb1b3bed
Alex Preiss <alexspreiss@gmail.com>
2019-09-26 08:23:14 -0700

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hi Ish</h1>
+    <p>This is my website kinda.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+  </Layout>
+)
+
+export default IndexPage
